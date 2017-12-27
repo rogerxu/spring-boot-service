61dae61ce4da958c045b7b0b0f7512f90619acde
Roger Xu <roger.xu.2007@gmail.com>
2017-12-27 19:42:36 +0800

    Add integration test

---
+++ src/test/java/com/example/demo/api/DemoControllerIT.java
@@ -0,0 +1,48 @@
+package com.example.demo.api;
+
+import com.example.demo.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@WebAppConfiguration
+public class DemoControllerIT {
+
+	private MediaType contentType = MediaType.APPLICATION_JSON_UTF8;
+	private MockMvc mockMvc;
+
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Before
+	public void setup() throws Exception {
+		this.mockMvc = webAppContextSetup(webApplicationContext).build();
+	}
+
+	@Test
+	public void greeting() throws Exception {
+		String name = "bob";
+
+		mockMvc.perform(get("/service/demo")
+			.param("name", name))
+			.andExpect(status().isOk())
+			.andExpect(content().contentType(contentType))
+			.andExpect(jsonPath("$.name", is(name)));
+	}
+}
